3aed620521adb0892378b0f77fb894c29026cf24
masa607 <jizomasa.06.07@gmail.com>
2025-04-16 21:36:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ labels = ['外向性', '計画性', '柔軟性', '論理的思考', '直感的�
           'ストレス耐性', '独立性', '協調性', '創造性', '感受性']
 
 DRIVE_FOLDER_ID = '1L-M95Ce-_4UYCdcmEAO1zNoX_BmUDcRb'
-SERVICE_ACCOUNT_FILE = 'neon-effect-456403-k6-004d1f6e4d0d.json'
+SERVICE_ACCOUNT_FILE = 'neon-effect-456403-k6-2fbf3676c38a.json'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 credentials = service_account.Credentials.from_service_account_file(
